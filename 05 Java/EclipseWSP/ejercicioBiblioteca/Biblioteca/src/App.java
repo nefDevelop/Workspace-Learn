7dcaa1f5e14584fb8aa90c5fb38b5096431a0d81
nef734 <nefdevelop@gmail.com>
2025-03-12 21:39:04 +0100

    Ejercicio objetos libros de DAW

--- 05 Java/EclipseWSP/ejercicioBiblioteca/Biblioteca/src/App.java
+++ 05 Java/EclipseWSP/ejercicioBiblioteca/Biblioteca/src/App.java
@@ -6,14 +6,26 @@ public class App {
 
         Libro jura = new Libro("1", "Sanderson", "Juramentada", "Nova", 2021, 1304);
         Libro caminReyes = new Libro("2", "Sanderonn", "Judramentada", "Nova", 2021, 1304);
-        Scanner read = new Scanner();
+        
+        Scanner read = new Scanner(System.in);
+
+        String isbn, autor, titulo, editorial;
+        int anyoPublicacion, pages;
 
         System.out.println("Isbn: ");
-        read.nextLine()
+        isbn = read.nextLine();
+        autor = read.nextLine();
+        titulo = read.nextLine();
+        editorial = read.nextLine();
+        anyoPublicacion = Integer.parseInt(read.nextLine());
+        pages = Integer.parseInt(read.nextLine());
+
+        Libro third = new Libro(isbn, autor, titulo, editorial, anyoPublicacion, pages);
 
         jura.showLibro();
         caminReyes.showLibro();
+        third.showLibro();
 
-
+        read.close();
     }
 }
