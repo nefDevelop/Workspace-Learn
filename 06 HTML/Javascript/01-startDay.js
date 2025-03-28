2543207916acaad11e037f8119c4a01113da7578
nef734 <nefdevelop@gmail.com>
2025-03-28 20:17:27 +0100

    Update moveProjecto

--- 06 HTML/Javascript/01-startDay.js
+++ 06 HTML/Javascript/01-startDay.js
@@ -1,8 +1,35 @@
-function messa(){
+//load json
 
-    alert('Hola');
-    let user = "neftali";
+//changePass
 
-    log.console(user)
+document.getElementById("password").addEventListener("input", function () {
+  let message = this.value;
+  console.log(message);
 
+  document.getElementById("passChange").textContent = message;
+});
+
+document
+  .getElementById("togglePassword")
+  .addEventListener("click", function () {
+    let passwordInput = document.getElementById("password");
+    if (passwordInput.type === "password") {
+      passwordInput.type = "text";
+      this.innerHTML = "&#128064;"; // Icono de ojo abierto
+    } else {
+      passwordInput.type = "password";
+      this.innerHTML = "🔒"; // Icono de ojo cerrado
+    }
+  });
+
+function copiarAlPortapapeles() {
+  const contenido = document.getElementById("contenido").textContent;
+  navigator.clipboard
+    .writeText(contenido)
+    .then(() => {
+      alert("Texto copiado al portapapeles!");
+    })
+    .catch((err) => {
+      console.error("Error al copiar: ", err);
+    });
 }
