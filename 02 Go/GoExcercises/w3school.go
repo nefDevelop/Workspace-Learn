bb6ae7a7ee3689b241a900573218cfea898cc5cd
nef734 <nefdevelop@gmail.com>
2025-04-04 12:22:44 +0200

    Go Morning

--- 02 Go/GoExcercises/w3school.go
+++ 02 Go/GoExcercises/w3school.go
@@ -1,5 +1,3 @@
-// Variables llamadas
-
 package main
 
 import (
@@ -10,12 +8,20 @@ var c = 3
 
 func main() {
 	// Variables
-	// var stu string
-	var dd, aa, vv int = 12, 232, 43
+	// var number = 12
+	// var number2 = -273465928
+	// var f = 3.14159265358979323846264338327950288419716939937510582097494
+	// var a bool = true
+	// var hola string = "Hola"
 
-	fmt.Println(dd)
-	fmt.Println(aa)
-	fmt.Println(vv)
+	// // FORMATO
+	// fmt.Printf("t:  %T  %v \n", number, number)
+	// fmt.Printf("t:  %T  %v \n", number2, number2)
 
-	// fmt.Println(stu + " " + dd)
+	var array1 = [3]int{2, 6, 0}
+	arr4 := [4]int{2: 4, 3: 5}
+
+	fmt.Println(len(array1))
+	fmt.Println(arr4)
+
 }
